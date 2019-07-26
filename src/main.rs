16cea8a4514bd8921d253adbcc1726b2945a0b79
Lukas Knuth <mr.luke.187@googlemail.com>
2019-07-26 20:47:03 +0200

    Added CLI parsing via clap.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,39 @@
 use audible_split;
 use std::process;
+extern crate clap;
+use clap::{Arg, App};
 
 fn main() {
+    let matches = App::new("Audbile Split")
+        .version("1.0")
+        .author("Lukas Knuth")
+        .about("Takes an Audible .aax file and splits it into an .mp3 file per chapter.")
+        .arg(Arg::with_name("input")
+            .short("i").long("input")
+            .value_name("FILE.aax")
+            .help("The Audbile .aax input file")
+            .takes_value(true)
+            .required(true)
+        )
+        .arg(Arg::with_name("activation_bytes")
+            .short("ab").long("activation_bytes")
+            .value_name("HEX")
+            .help("The activation bytes to decode the given Audbile .aax file")
+            .takes_value(true)
+            .required(true)
+        )
+        .arg(Arg::with_name("output")
+            .short("o").long("output")
+            .value_name("FOLDER")
+            .help("The output folder which contains all transcoded .mp3 files")
+            .default_value("output/")
+            .takes_value(true)
+        ).get_matches();
     
-    let input = String::from("test.aax");
-    let output = String::from("out/");
-    let activation_bytes = String::from("something");
+    let input = matches.value_of("input").unwrap().to_string();
+    let output = matches.value_of("output").unwrap().to_string();
+    let activation_bytes = matches.value_of("activation_bytes").unwrap().to_string();
 
     let result_code = audible_split::run(input, output, activation_bytes);
     process::exit(result_code);
-
 }
